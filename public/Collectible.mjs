ee5e684ed2aee40402af065de14f6b1b91bed9ba
hsesay969 <henrys20251@gmail.com>
2025-05-06 18:21:42 +0300

    added game logic

--- public/Collectible.mjs
+++ public/Collectible.mjs
@@ -1,13 +1,14 @@
 class Collectible {
   constructor({x, y, value, id}) {
-    this.x = x;
-    this.y = y;
-    this.value = value || 1;
-    this.id = id || Date.now();
-    this.radius = 10; // For collision detection
+
   }
+
 }
 
+/*
+  Note: Attempt to export this for use
+  in server.js
+*/
 try {
   module.exports = Collectible;
 } catch(e) {}
